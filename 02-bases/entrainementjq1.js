fbb2e7eff0e2833112e8ee60166e790b86720291
quiiwi <kilic.aslan@lepoles.com>
2018-07-10 14:29:20 +0200

    saluté

---
+++ 02-bases/entrainementjq1.js
@@ -0,0 +1,22 @@
+//Ce fichier est en jQuery
+/* 
+    Document ready :
+    Les deux lignes suivantes ont la même signification : exécuter le code JS après avoir changé le code HTML. Par conséquent, le lien vers la page JS peut se faire n'importe où dans la page HTML (dans le <head> ou avant la fermeture de <body>).
+
+    $(function(){
+        //ici vous mettez tout votre code jQuery
+    });
+
+    ou encore, autre syntaxe :
+    $(document).ready(function(){
+        //ici vous mettez tout votre code jQuery
+    });
+
+*/
+
+$(function(){ //ce code s'éxécutera une fois le document HTML totalement chargé
+
+
+
+
+}); //FIN DU DOCUMENT READY (à ne pas supprimer)
